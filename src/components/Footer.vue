37f691ca486002f2645c65fae311428ffcebe85a
elton lima <eltonathayde2@gmail.com>
2022-03-23 22:53:44 -0300

    navbar e footer finalizados

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-    <p>footer</p>
+<footer id="footer">
+    <p>Make Your Burger &copy; 2021</p>
+</footer>
+   
 </template>
 
 <script>
@@ -7,3 +10,15 @@ export default{
     name:"Footer"
 }
  </script>
+<style  scoped>
+#footer{
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+</style>
