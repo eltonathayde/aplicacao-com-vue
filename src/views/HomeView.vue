2a358f83d022a80e46600655c3e10c9128c355a5
elton lima <eltonathayde2@gmail.com>
2022-03-23 23:25:57 -0300

    banner finalizado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
+<Banner/>
   <div class="main-container">
-   <h1>Home</h1>
+   <h1>Monte o seu Burger:</h1>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
 
+
+export default{
+  name:"Home",
+  components:{
+    Banner
+  }
+}
 </script>
